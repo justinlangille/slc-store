f369791ceea0af2d7216723dc6ec370467569528
Justin Langille <justin.langille@shopify.com>
2017-07-24 09:16:54 -0400

    ajax cart running

---
+++ app/models/order.rb
@@ -0,0 +1,11 @@
+class Order < ApplicationRecord
+
+  enum pay_type: {
+    "Cheque" =>         0,
+    "Credit Card" =>    1,
+    "Purchase Order" => 2,
+  }
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+end
